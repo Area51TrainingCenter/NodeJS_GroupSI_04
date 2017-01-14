009d121c41d0a7c31eec1c9017b6c6d2d68c3d06
Admin <admin@A51-MM-16.local>
2017-01-14 15:36:40 -0500

    Clase06

--- Clase06/01-api/assets/js/appControladores.js
+++ Clase06/01-api/assets/js/appControladores.js
@@ -1,6 +1,7 @@
 app.controller("controlador",["http", function(http){
 
 	this.alumnos = []
+	this.alumno = {}
 
 	var self = this
 
@@ -8,9 +9,89 @@ app.controller("controlador",["http", function(http){
 		.listar()
 		.then(function(registros){
 			self.alumnos = registros.data
+			self.alumnos.forEach(function(alumno){
+				alumno.editando = false
+			})
 		})
 		.catch(function(err){
 			console.log(err)
 		})
+
+	this.detallar = function(registro) {
+		http
+			.detallar(registro.idUsuario)
+			.then(function(registros){
+				self.alumno = registros.data[0]
+			})
+			.catch(function(err){
+				console.log(err)
+			})
+	},
+
+	this.insertar = function(nombre){
+		var data = {nombre: nombre}
+		http
+			.insertar(data)
+			.then(function(registros){
+				return http.listar()
+			})
+			.then(function(registros){
+				self.alumnos = registros.data
+				self.alumnos.forEach(function(alumno){
+					alumno.editando = false
+				})
+			})
+			.catch(function(err){
+				console.log(err)
+			})		
+	},
+
+	this.actualizar = function(registro){
+		var data = {nombre: registro.nombre}
+
+		http
+			.actualizar(data, registro.idUsuario)
+			.then(function(registros){
+				return http.listar()
+			})
+			.then(function(registros){
+				self.alumnos = registros.data
+				self.alumnos.forEach(function(alumno){
+					alumno.editando = false
+				})
+			})
+			.catch(function(err){
+				console.log(err)
+			})		
+	},
+
+	this.eliminar = function(registro){
+
+		if(!confirm("¿Esta seguro de querer eliminar?"))  return
+
+
+		http
+			.eliminar(registro.idUsuario)
+			.then(function(registros){
+				return http.listar()
+			})
+			.then(function(registros){
+				self.alumnos = registros.data
+				self.alumnos.forEach(function(alumno){
+					alumno.editando = false
+				})
+			})
+			.catch(function(err){
+				console.log(err)
+			})		
+	}
+
+
+
+
+
+
+
+
 
 }])
--- Clase06/01-api/assets/js/appFactorias.js
+++ Clase06/01-api/assets/js/appFactorias.js
@@ -3,6 +3,22 @@ app.factory("http", ["$http", function(http){
 
 	obj.listar = function(){
 		return http.get("http://localhost:1337/usuarios")
+	},
+
+	obj.detallar = function(id){
+		return http.get("http://localhost:1337/usuarios/" + id)
+	},
+
+	obj.insertar = function(registro) {
+		return http.post("http://localhost:1337/usuarios", registro)
+	},
+
+	obj.actualizar = function(registro, id){
+		return http.put("http://localhost:1337/usuarios/" + id, registro)
+	},
+
+	obj.eliminar = function(id) {
+		return http.delete("http://localhost:1337/usuarios/" + id)
 	}
 
 	return obj

f495227cb91abcb51e4b051c39fc49b82c7b6cdc
Admin <admin@A51-MM-16.local>
2017-01-28 10:35:42 -0500

    Clase08

---
+++ Clase08/01-sincronizacion/api/controllers/MonitoreoController.js
@@ -0,0 +1,82 @@
+/**
+ * MonitoreoController
+ *
+ * @description :: Server-side logic for managing monitoreos
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+	suscripcion: function(req, res, next){
+		Monitoreo
+			.find()
+			.then(function(registros){
+				if(req.isSocket){
+					console.log("suscrito")
+					Monitoreo.subscribe(req, registros)
+					Monitoreo.watch(req)
+					res.json(registros)
+				}
+
+
+			})
+			.catch(function(err){
+				res.negotiate(err)
+			})
+	},
+
+	actualizar: function(req, res, next){
+		var filtro = {id: req.params.id}
+		var registro = req.allParams()
+
+		Monitoreo
+			.update(filtro, registro)
+			.then(function(registros){
+				Monitoreo.publishUpdate(registros[0].id, registros[0])
+				res.ok()
+			})
+			.catch(function(err){
+				res.negotiate(err)
+			})
+
+	},
+
+	insertar: function(req, res, next){
+		var data = req.allParams()
+
+		Monitoreo
+			.create(data)
+			.then(function(registro){
+				Monitoreo.publishCreate(registro, req)
+				res.ok()
+			})
+			.catch(function(err){
+				res.negotiate(err)
+			})
+	},
+
+	eliminar: function(req, res, next) {
+		var filtro = {id: req.params.id}
+
+		Monitoreo
+			.destroy()
+			.where(filtro)
+			.then(function(registros){
+				Monitoreo.publishDestroy(registros[0].id, req)
+				res.ok()
+			})
+			.catch(function(err){
+				res.negotiate(err)
+			})
+
+
+	}
+
+
+
+
+
+
+	
+};
+

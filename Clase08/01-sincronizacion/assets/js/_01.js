1840ee7cae6149004a17afbc227ab70ce1c3ccf6
Admin <admin@A51-MM-16.local>
2017-01-28 11:57:32 -0500

    Clase08

--- Clase08/01-sincronizacion/assets/js/_01.js
+++ Clase08/01-sincronizacion/assets/js/_01.js
@@ -1,7 +1,49 @@
-io.socket.get("/suscripcion", function(registros){
-	console.log(registros)
-})
+var app = angular.module("app", [])
 
-io.socket.on("monitoreo", function(obj){
-	console.log(obj)
-})
+app.controller("ambulancias", ["$scope", function($scope){
+/*	this.lista = [
+		{id: 1, medico: "medico1", piloto: "piloto1", paramedico: "paramedico1"},
+		{id: 2, medico: "medico2", piloto: "piloto2", paramedico: "paramedico2"},
+		{id: 3, medico: "medico3", piloto: "piloto3", paramedico: "paramedico3"},
+		{id: 4, medico: "medico4", piloto: "piloto4", paramedico: "paramedico4"},
+		{id: 5, medico: "medico5", piloto: "piloto5", paramedico: "paramedico5"},
+		{id: 6, medico: "medico6", piloto: "piloto6", paramedico: "paramedico6"},
+		{id: 7, medico: "medico7", piloto: "piloto7", paramedico: "paramedico7"},
+		{id: 8, medico: "medico8", piloto: "piloto8", paramedico: "paramedico8"},
+		{id: 9, medico: "medico9", piloto: "piloto9", paramedico: "paramedico9"}
+	]*/
+
+	this.lista = []
+
+	var self = this
+
+	io.socket.get("/suscripcion", function(registros){
+		$scope.$apply(function(){
+			self.lista = registros	
+		})
+		console.log(registros)
+	})
+
+	io.socket.on("monitoreo", function(obj){
+		console.log(obj)
+		$scope.$apply(function(){
+			switch(obj.verb) {
+				case "updated":
+					self.lista.forEach(function(registro){
+						if(registro.id==obj.id) {
+							registro.medico = obj.data.medico
+							registro.piloto = obj.data.piloto
+							registro.paramedico = obj.data.paramedico
+						}
+					})
+					break
+			}
+		})
+	})
+
+
+}])
+
+
+
+
